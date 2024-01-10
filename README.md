76ddb25749240c3897a67a9a5258f1fff0f8b68c
luoyuqinlaura <84111527+luoyuqinlaura@users.noreply.github.com>
2024-01-09 17:44:15 -0800

    Morgan Logger

---
+++ README.md
@@ -0,0 +1,11 @@
+# StoryBooks
+
+## Install dependencies + Express set up
+
+## Connect database
+
+1. store mongo uri in the config file
+
+## Morgan Logger
+
+when there is a request to the page, it just shows down the console
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 const connectDB = require('./config/db');
 //Load config
 dotenv.config({ path: './config/config.env' });
@@ -7,6 +8,9 @@ dotenv.config({ path: './config/config.env' });
 connectDB();
 
 const app = express();
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
